ccfd3c7a899fbddb3ca54a8a3d37d1466d62f40f
minhthu4246 <habsbjwb@gmail.com>
2024-04-17 09:14:00 +0700

    Search Page and App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
-  return (
-    <div>
-      <h1>My App</h1>
-    </div>
-  );
+import './App.scss';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Home, Product, Cart, Search } from './pages/index';
+
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import store from './reducer/store';
+import { Provider } from 'react-redux';
+
+function App() {
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Header />
+                    <Sidebar />
+
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/product/:id" element={<Product />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/search/:searchTerm" element={<Search />} />
+                    </Routes>
+
+                    <Footer />
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
 }
+
+export default App;
