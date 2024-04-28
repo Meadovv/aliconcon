8c0c23150db7bfd8c245e8e64c4a4795b05c2d1a
Meadow <hoangthanh2k3@gmail.com>
2024-04-28 22:58:04 +0700

    Ghép chức năng đăng nhập, đăng ký, đăng xuất, lấy danh sách người dùng vào shop-center

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
--- shop-center/src/reducer/actions/auth.slice.js
+++ shop-center/src/reducer/actions/auth.slice.js
@@ -3,18 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        shop: null,
-        token: null
+        shop: null
     },
     reducers: {
         setAuth: (state, action) => {
-            state.shop = action.payload.shop;
-            state.token = action.payload.token;
-            console.log("aaa");
+            console.log(action.payload);
+            state.shop = action.payload;
         },
         removeAuth: (state) => {
             state.shop = null;
-            state.token = null;
         },
     }
 });
@@ -23,6 +20,5 @@ export const { setAuth, removeAuth } = authSlice.actions;
 
 // Selector function to access shop and token
 export const selectShop = (state) => state.auth.shop;
-export const selectToken = (state) => state.auth.token;
 
 export default authSlice.reducer;
