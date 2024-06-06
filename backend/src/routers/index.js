1553fd542a46b805452b8842f5190872d973e047
Meadovv <hoangthanh2k3@gmail.com>
2024-06-06 14:29:15 +0000

    Deploy Test

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -12,9 +12,9 @@ router.get('/', (req, res) => {
 
 router.use(AuthenticationMiddleware.checkCookies);
 
-router.use('/api/v1/access', require('./access.router'));
-router.use('/api/v1/shop', require('./shop.router'))
-router.use('/api/v1/user', require('./user.router'));
-router.use('/api/v1/image', require('./image.router'));
+router.use('/v1/access', require('./access.router'));
+router.use('/v1/shop', require('./shop.router'))
+router.use('/v1/user', require('./user.router'));
+router.use('/v1/image', require('./image.router'));
 
 module.exports = router;
